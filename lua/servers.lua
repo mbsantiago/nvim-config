065cc93450bfdde40b7bfad283641f5cc8b9018d
Santiago Martinez <santiago.mbal@gmail.com>
2023-03-04 18:17:49 +0000

    Attempting a good combination of LSP capabilities

--- lua/servers.lua
+++ lua/servers.lua
@@ -19,6 +19,9 @@ return {
       },
     },
   },
+  pyright = {
+    navic_disable = true,
+  },
   pylsp = {
     cmd = { "pylsp" },
     python = {
@@ -26,21 +29,32 @@ return {
     },
     pylsp = {
       plugins = {
+        -- Formatting is provided by Black through null-ls
         autopep8 = { enabled = false },
         yapf = { enabled = false },
+        -- Linters
+        ruff = { enabled = true },
+        flake8 = { enabled = false },
+        pyflakes = { enabled = false },
         pylint = { enabled = false },
-        flake8 = { enabled = true },
         pydocstyle = { enabled = false },
         pycodestyle = { enabled = false },
         mccabe = { enabled = false },
-        rope_autoimport = { enabled = false },
-        rope_completion = { enabled = true },
+        -- Most language capabilities are provided by pyright
+        jedi_hover = { enabled = false },
+        jedi_definition = { enabled = false },
+        jedi_symbols = { enabled = false },
+        jedi_references = { enabled = false },
+        jedi_signature_help = { enabled = false },
+        -- Jedi completion is better though
+        jedi_completion = { enabled = true, fuzzy = true },
+        rope_autoimport = { enabled = true, memory = true },
+        -- Rope completion is not as good as Jedi
+        rope_completion = { enabled = false },
+        -- Type checking is provided by pyright
         pyls_mypy = { enabled = false },
       },
     },
-  },
-  pyright = {
-    navic_disable = true,
   },
   texlab = {
     texlab = {
