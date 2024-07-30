95dd69ed93384c04f9700ab2bfb70ed3788f30f0
mbsantiago <santiago.mbal@gmail.com>
2024-07-30 12:53:19 +0100

    More formatters

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,6 +3,13 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        typst = { "typstyle" },
+        toml = { "taplo" },
+        typescriptreact = { "prettierd", "rustywind" },
+        javascriptreact = { "prettierd", "rustywind" },
+        html = { "prettierd", "rustywind" },
+        markdown = { "prettierd" },
+        json = { "jq" },
         sh = { "shfmt" },
         bash = { "shfmt" },
         bibtex = { "bibtex-tidy" },
@@ -10,8 +17,13 @@ return {
         groovy = { "npm-groovy-lint" },
         typescript = { "prettierd" },
         javascript = { "prettierd" },
-        python = { "ruff", lsp_format = "fallback" },
+        python = { "ruff_format", "ruff_organize_imports", "ruff_fix" },
         lua = { "stylua" },
+        ["_"] = { "trim_whitespace" },
+      },
+      notify_no_formatters = true,
+      default_format_opts = {
+        lsp_format = "fallback",
       },
     },
     lazy = true,
