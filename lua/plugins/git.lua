1f29008833b3159d1fc12d653017caac8382a4c3
mbsantiago <santiago.mbal@gmail.com>
2024-08-18 14:55:56 +0100

    Octo: define keybindings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,42 +17,42 @@ return {
     },
     keys = {
       {
-        "<leader>gj",
+        "<leader>gsj",
         "<cmd>lua require('gitsigns').next_hunk()<cr>",
         desc = "Git Next Hunk",
       },
       {
-        "<leader>gk",
+        "<leader>gsk",
         "<cmd>lua require('gitsigns').prev_hunk()<cr>",
         desc = "Git Prev Hunk",
       },
       {
-        "<leader>gp",
+        "<leader>gsp",
         "<cmd>lua require('gitsigns').preview_hunk()<cr>",
         desc = "Git Preview Hunk",
       },
       {
-        "<leader>gr",
+        "<leader>gsr",
         "<cmd>lua require('gitsigns').reset_hunk()<cr>",
         desc = "Git Reset Hunk",
       },
       {
-        "<leader>gR",
+        "<leader>gsR",
         "<cmd>lua require('gitsigns').reset_buffer()<cr>",
         desc = "Git Reset Buffer",
       },
       {
-        "<leader>gs",
+        "<leader>gss",
         "<cmd>lua require('gitsigns').stage_hunk()<cr>",
         desc = "Git Stage Hunk",
       },
       {
-        "<leader>gu",
+        "<leader>gsu",
         "<cmd>lua require('gitsigns').undo_stage_hunk()<cr>",
         desc = "Git Undo Stage Hunk",
       },
       {
-        "<leader>gS",
+        "<leader>gsS",
         "<cmd>lua require('gitsigns').stage_buffer()<cr>",
         desc = "Git Stage Buffer",
       },
@@ -70,27 +70,27 @@ return {
     },
     keys = {
       {
-        "<leader>gd",
+        "<leader>gdo",
         "<cmd>DiffviewOpen HEAD -- %<cr>",
         desc = "Diff Current File",
       },
       {
-        "<leader>gD",
+        "<leader>gda",
         "<cmd>DiffviewOpen<cr>",
         desc = "Diff All Files",
       },
       {
-        "<leader>gh",
+        "<leader>gdh",
         "<cmd>DiffviewFileHistory %<cr>",
         desc = "Current File History",
       },
       {
-        "<leader>gH",
+        "<leader>gdH",
         "<cmd>DiffviewFileHistory %<cr>",
         desc = "Git History",
       },
       {
-        "<leader>gc",
+        "<leader>gdc",
         "<cmd>DiffviewClose<cr>",
         desc = "Close Diffview",
       },
@@ -111,6 +111,61 @@ return {
         },
       })
     end,
+    keys = {
+      {
+        "<leader>fo",
+        "<cmd>Octo search<cr>",
+        desc = "Find GitHub issues or PRs",
+      },
+      {
+        "<leader>gio",
+        "<cmd>Octo issue list<cr>",
+        desc = "[l]ist Git Hub issues",
+      },
+      {
+        "<leader>giC",
+        "<cmd>Octo issue close<cr>",
+        desc = "[C]lose issue",
+      },
+      {
+        "<leader>gic",
+        "<cmd>Octo issue create<cr>",
+        desc = "[c]reate a new issue",
+      },
+      { "<leader>gpl", "<cmd>Octo pr list<cr>", desc = "[l]ist pull requests" },
+      { "<leader>gpe", "<cmd>Octo pr edit<cr>", desc = "[e]dit pull requests" },
+      {
+        "<leader>gpc",
+        "<cmd>Octo pr create<cr>",
+        desc = "[c]create pull requests",
+      },
+      {
+        "<leader>gpC",
+        "<cmd>Octo pr close<cr>",
+        desc = "[C]lose pull requests",
+      },
+      { "<leader>grs", "<cmd>Octo review start<cr>", desc = "[s]tart Review" },
+      {
+        "<leader>grS",
+        "<cmd>Octo review submit<cr>",
+        desc = "[S]ubmit review",
+      },
+      {
+        "<leader>grr",
+        "<cmd>Octo review resume<cr>",
+        desc = "[r]esume review",
+      },
+      {
+        "<leader>grd",
+        "<cmd>Octo review discard<cr>",
+        desc = "[d]iscard review",
+      },
+      {
+        "<leader>grp",
+        "<cmd>Octo review comments<cr>",
+        desc = "View [p]ending Comments",
+      },
+    },
   },
   {
     "kdheepak/lazygit.nvim",
